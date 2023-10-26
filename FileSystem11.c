a307165b93a629a3b3c3656c2b8dbe847852688a
Pratik Chavan <106006986+PratikChavan19@users.noreply.github.com>
2023-10-26 18:12:00 +0530

    Add files via upload

---
+++ FileSystem11.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<fcntl.h>
+
+int main(int argc, char *argv[])
+{
+	int fd = 0
+	char Buffer[20] = {'\0'};
+	int Ret = 0;
+	
+	if(argc != 2)
+	{
+		printf("Insufficient Parameters");
+		return -1;
+	}
+	
+	
+
+	return 0;
+}
